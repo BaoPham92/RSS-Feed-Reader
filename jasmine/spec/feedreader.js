77bcdbcdc4837c79e5b91cc881bc3fea92110c0b
Cuza <baopham92@yahoo.com>
2018-09-24 00:47:26 -0400

    Expectations for name property added for second spec.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -45,6 +45,14 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+
+        it('name is defined, not empty.', function () {
+
+            allFeeds.forEach((feed) => {
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            })
+        })
     });
 
 
