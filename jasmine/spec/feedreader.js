82deb0fb50e9025bf18769a22b9efe48804bb424
Cuza <baopham92@yahoo.com>
2018-09-27 19:35:24 -0400

    Additional comments to (RSS Feeds Suite) for clarity.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,36 +1,16 @@
-/* feedreader.js
- *
- * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
- */
-
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
- */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+
+    // A suite representing tests for app.js > allFeeds obj (.url && .name) === defined && not empty.
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
+
+        // Spec testing if app.js > allFeeds obj === defined && not empty.
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
 
+        // Spec testing if (.url) prop is defined && (.url) prop not empty.
         it('URL is defined, not empty.', function() {
 
             allFeeds.forEach((feed)=> {
@@ -39,11 +19,7 @@ $(function() {
             })
         })
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
-
+        // Spec testing if (.name) prop is defined && (.name) prop not empty.
         it('name is defined, not empty.', function () {
 
             allFeeds.forEach((feed) => {
