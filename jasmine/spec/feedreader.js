16208c4912e635ce5e27f4b11f8f814e3a0bdf86
Cuza <baopham92@yahoo.com>
2018-09-27 19:26:34 -0400

    Additional comments for (Menu Suite) for clarity.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -53,9 +53,7 @@ $(function() {
         })
     });
 
-
-    /* TODO: Write a new test suite named "The menu" */
-
+    // A suite representing tests for menu logic css logic.
     describe('The menu', function() {
 
         let menu = document.querySelector('body');
@@ -63,24 +61,14 @@ $(function() {
         beforeEach(function() {
             menu = document.querySelector('body');
         })
-      
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
 
+        // Spec testing if (menu) === hidden || not.
         it('should be able to hide menu by default.', function() {
 
             expect(menu.classList.contains('menu-hidden')).toBe(true);
         })
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
-
+        // Spec testing if (menu display) === true || false
         it('should be able to ensure menu visibility when clicking menu icon.', function() {
             let icon = document.querySelector('.menu-icon-link');
 
